0152c5094b07edfd67fa7130168439b817454894
Jayant Mishra <jayant.m.1988@gmail.com>
2024-07-10 20:39:36 +0530

    Changed code to show data in form of template

--- NetHttpHandsOn/EX1/main.go
+++ NetHttpHandsOn/EX1/main.go
@@ -14,10 +14,16 @@ Have the "/me/" route print out your name
 package main
 
 import (
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
 
 func main() {
 	http.HandleFunc("/", defaultHandler)
@@ -27,19 +33,40 @@ func main() {
 }
 
 func defaultHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "This is default handler")
+	url_query := req.URL.Query()
+	name := url_query.Get("name")
+	data := data{
+		DataType: "Default",
+		Data:     name,
+	}
+	tpl.Execute(res, data)
 }
 
 func dogHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "This is dog handler")
+	name := req.URL.Query().Get("name")
+
+	data := data{
+		DataType: "Dog",
+		Data:     name,
+	}
+	tpl.Execute(res, data)
 }
 
 func meHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "This is me hadler")
 	log.Println("request object", req)
 	url_query := req.URL.Query()
 	name := url_query.Get("name")
 	log.Println("Passed name param value", name)
 	header_val := req.Header.Get("X-Custom-Header")
 	log.Println("Header value ", header_val)
+	data := data{
+		DataType: "Me",
+		Data:     name,
+	}
+	tpl.Execute(res, data)
+}
+
+type data struct {
+	DataType string
+	Data     string
 }
